1de6bad0da1ef56e0f7d155715ddb872eb6894c0
NazarRudyi <Rudyi.Nazar@gmail.com>
2016-05-13 17:46:38 +0300

    Create Maun Activity and layout for its

---
+++ app/src/main/java/com/succeeder/remindme/MainActivity.java
@@ -0,0 +1,16 @@
+package com.succeeder.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by succeeder on 13.05.16.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
